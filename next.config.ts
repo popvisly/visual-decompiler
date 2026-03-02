932999139798181eb24d8fc1cc6b97ea75b58b94
Red Claw <redclaw@villainsatlarge.com>
2026-03-02 19:32:54 +1100

    build: remove 34MB yt-dlp binary from src and optimize Sentry config to fix Vercel deployment error

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,8 @@ const nextConfig: NextConfig = {
   // supports ad creatives up to 20MB
   // Note: in Next 15, bodySizeLimit might have moved or is handled differently
   // but if it's unrecognized, we'll omit it for now or check experimental
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '20mb',
-    }
+  serverActions: {
+    bodySizeLimit: '20mb',
   },
   async redirects() {
     return [
@@ -25,4 +23,7 @@ export default withSentryConfig(nextConfig, {
   silent: true,
   org: "paul-ikins",
   project: "visual-decompiler",
+  widenClientFileUpload: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
 });
