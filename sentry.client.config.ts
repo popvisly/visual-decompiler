eaa31bffb3a4e5aab38caab7a5fd26db36bde4e0
Red Claw <redclaw@villainsatlarge.com>
2026-03-02 17:26:56 +1100

    chore: enable production Sentry monitoring with DSN

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,8 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
-// Sentry temporarily disabled for launch - will re-enable after proper testing
-// Sentry.init({
-//     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-//     tracesSampleRate: 1.0,
-//     debug: false,
-// });
+Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    debug: false,
+});
